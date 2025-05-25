cb9703c630ddb0c13c12e7ce44b3ea43b838b98a
Abbah A <25776788+anohabbah@users.noreply.github.com>
2025-05-25 09:51:46 +0200

    Add initial Spring Boot project setup
    Set up a new Spring Boot project with WebFlux, Actuator, and MongoDB Reactive dependencies. Included Gradle build configuration, Docker Compose for MongoDB, and basic application and test classes.

---
+++ src/main/java/dev/abbah/supervision/eventtype/EventTypeSupervisionApplication.java
@@ -0,0 +1,13 @@
+package dev.abbah.supervision.eventtype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTypeSupervisionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EventTypeSupervisionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/abbah/supervision/eventtype/EventTypeSupervisionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abbah.supervision.eventtype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTypeSupervisionApplicationTests {
+
+  @Test
+  void contextLoads() {  // Noncompliant - method is empty
+  }
+
+}
